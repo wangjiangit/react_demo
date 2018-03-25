1fffea178bc8bf565bc36096defa0fff1a62119e
wangjian <jiji_12342006@163.com>
2018-03-25 10:23:43 +0800

    组合VS继承

--- src/App.js
+++ src/App.js
@@ -1,54 +1,103 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TimeComponent from './TimeComponent';  //引入组件TimeComponent
 import NameForm from './NameForm';
 import Calculator from './Calculator';
 
-function WarningBanner(props){
+function WarningBanner(props) {
 
-    if(!props.warn){
-       return null;
+    if (!props.warn) {
+        return null;
     }
     return (
         <span id="warnBanner">警告</span>
     );
 }
 
-function ItemList(){
-    let nums=[1,2,3,4];
-    let numsList= nums.map((num)=>{
+function ItemList() {
+    let nums = [1, 2, 3, 4];
+    let numsList = nums.map((num)=> {
         return <li key={num.toString()}>{num}</li>  //为每个元素分配key属性，是让React更好的确定那个元素发生变化
     });
     return <ul>{numsList}</ul>;
 }
 
 
-function UserList(){
-    let userArr=[
-        {"id":1,"name":"张三","age":21,"sex":"男"},
-        {"id":2,"name":"李四","age":22,"sex":"男"},
-        {"id":3,"name":"王五","age":21,"sex":"女"},
+function UserList() {
+    let userArr = [
+        {"id": 1, "name": "张三", "age": 21, "sex": "男"},
+        {"id": 2, "name": "李四", "age": 22, "sex": "男"},
+        {"id": 3, "name": "王五", "age": 21, "sex": "女"},
     ];
 
     return (
         <ul>
-            {userArr.map((userObj)=>{
-              return  <li key={userObj.id}>{userObj.name}</li>
+            {userArr.map((userObj)=> {
+                return <li key={userObj.id}>{userObj.name}</li>
             })}
         </ul>
     );
 }
+
+
+function FancyBorder(props) {
+
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children}
+
+        </div>
+    );
+}
+
+function WelcomeDialog() {
+
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                Welcome
+            </h1>
+            <p className="Dialog-message">
+                Thank you for visiting our spacecraft!
+            </p>
+        </FancyBorder>
+    );
+
+}
+
+function Contact() {
+    return <h4>Name:wangjian</h4>
+}
+
+function Info() {
+    return <h6> address : Hangzhou</h6>;
+}
+function SplitPane(props) {
+    return (
+        <div className="SplitPane">
+            <div className="SplitPane-left">
+                {props.left}
+            </div>
+            <div className="SplitPane-right">
+                {props.right}
+            </div>
+
+        </div>
+    );
+
+}
+
 class App extends Component {
     static app_url = 'http://www.baidu.com';
 
     render() {
-        const isLogin=true;
-        let greetStr=null;
-        if(isLogin){
-            greetStr='Hello ,David';
-        }else{
-            greetStr='Hello ,Guest';
+        const isLogin = true;
+        let greetStr = null;
+        if (isLogin) {
+            greetStr = 'Hello ,David';
+        } else {
+            greetStr = 'Hello ,Guest';
         }
         return (
             <div className="App">
@@ -64,13 +113,15 @@ class App extends Component {
                 <TimeComponent  />
                 {greetStr}
                 {true && <h3>与运算符</h3>}
-                {isLogin ?'Login':'UnLogin'}
+                {isLogin ? 'Login' : 'UnLogin'}
 
-                <WarningBanner warn={true} />
+                <WarningBanner warn={true}/>
                 <ItemList />
                 <UserList />
                 <NameForm/>
                 <Calculator/>
+                <WelcomeDialog/>
+                <SplitPane left={<Contact/>} right={<Info/>}/>
             </div>
         );
     }
